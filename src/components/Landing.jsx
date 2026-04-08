fd69087536a2b4b239df137a533b03583c3c9e08
Julian Crenshaw <julian.gold.crenshaw@gmail.com>
2026-04-08 13:53:12 -0500

    Replace link with button for smooth scrolling

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -9,9 +9,16 @@ const Landing = () => {
         <div className="header__description">
             <h1>Satisfy your sweet tooth with our delicious Desserts!</h1>
             <h2>Find your sweets in our <span className="blue">Inventory</span></h2>
-            <a href="#features">
-                <button className="btn">Browse Deserts</button>
-            </a>
+            <button
+            className="btn"
+            onClick={() => {
+                const section =
+                document.getElementById("features");
+                section?.scrollIntoView({ behavior:
+                    "smooth" });
+            }}
+            >Browse Books
+            </button>
         </div>
         <figure className="header__img--wrapper">
             <img src={ Party } alt="" />
